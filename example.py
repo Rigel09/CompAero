7565aba0fa634ce2f5e09427cd0fdafd6f0aa69d
JNewton <jnnewton@hiwaay.net>
2022-01-06 22:43:51 -0600

    Updated MD

--- example.py
+++ example.py
@@ -25,8 +25,8 @@ if __name__ == "__main__":
     ##########################################################################
     #################### Normal Shock Example  ###############################
     ##########################################################################
-    # flow = NormalShockRelations(gamma=1.4, m2=0.5130)
-    # print(flow)
+    flow = NormalShockRelations(gamma=1.4, m2=0.5130)
+    print(flow)
 
     ##########################################################################
     #################### Oblique Shock Example  ##############################
@@ -40,17 +40,17 @@ if __name__ == "__main__":
     #################### Fanno Flow Example  #################################
     ##########################################################################
     # Fanno Flow Example
-    # t1 = 300
-    # p1 = 1
-    # po1 = IsentropicRelations.calc_p0_p(3, 1.4) * p1
-    # flow = FannoFlowRelations(1.4, po_poSt=4.23456790)
-    # flow.applyPipeParameters(0.4, 11, 0.005)
-    # print()
-    # print(flow)
-    # print()
-    # print("T2: ", t1 * flow.t2_t1)
-    # print("P2: ", p1 * flow.p2_p1)
-    # print("Po2: ", po1 * flow.po2_po1)
+    t1 = 300
+    p1 = 1
+    po1 = IsentropicRelations.calc_p0_p(3, 1.4) * p1
+    flow = FannoFlowRelations(1.4, po_poSt=4.23456790)
+    flow.applyPipeParameters(0.4, 11, 0.005)
+    print()
+    print(flow)
+    print()
+    print("T2: ", t1 * flow.t2_t1)
+    print("P2: ", p1 * flow.p2_p1)
+    print("Po2: ", po1 * flow.po2_po1)
 
     # Plot length and diameter combinations to slow a mach 3 flow down to a mach 2.5 flow at the exit
     # f4ld = FannoFlowRelations.calc4FLSt_D_FromMach(1.5, 1.4)
@@ -75,10 +75,10 @@ if __name__ == "__main__":
     ##########################################################################
     #################### Rayleigh Flow Example  ##############################
     ##########################################################################
-    # flow = RayleighFlowRelations(1.4, mach=1.5)
-    # print()
-    # flow.simulateHeatAddition(1000, 275.2, 287)
-    # print(flow)
+    flow = RayleighFlowRelations(1.4, mach=1.5)
+    print()
+    flow.simulateHeatAddition(1000, 275.2, 287)
+    print(flow)
 
     ##########################################################################
     #################### Prandtl Meyer Example  ##############################
