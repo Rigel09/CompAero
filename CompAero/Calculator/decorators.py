e9a18a2384ec410f51ee921b4a8225701dc66b71
Rigel09 <joshua.newton102@gmail.com>
2023-11-26 09:42:46 -0600

    Updated the calculator to Pep8

--- CompAero/Calculator/decorators.py
+++ CompAero/Calculator/decorators.py
@@ -1,7 +1,22 @@
+"""
+This module defines some common decorators used in the comp aero UI
+"""
+
+
 from typing import Callable
 
+
 def error_message_decorator(func) -> Callable:
-    
+    """
+    Decorator that wraps a function in  try / catch statement and shows that error in a pop up
+    message window in the UI
+
+    Args:
+        func (callable): The function to be wrapped
+
+    Returns: The wrapped function
+    """
+
     def _wrapped_func(self):
         result = None
         try:
@@ -9,4 +24,6 @@ def error_message_decorator(func) -> Callable:
         except ValueError as e:
             self.show_error(f"Error Occured! \n {e}")
         return result
+
     return _wrapped_func
+
